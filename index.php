37d64203cbec67a91080ee918f08ffeb0bb0d1c0
lucadisanto7 <lucadisanto47@gmail.com>
2024-09-11 18:30:16 +0200

    [UPD] Added index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Todo</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+</head>
+
+<body>
+    
+</body>
+
+</html>
